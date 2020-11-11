8ff4612c7046d1697d43d1b9faf70e14ae482fef
rodrigo.leoncio <rodrigo.leoncio@sicoob.com.br>
2020-11-11 00:40:12 -0300

    testes 6/7

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,51 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+   const transactionsRepository = getCustomRepository(TransactionsRepository);
+   const transactions = await transactionsRepository.find();
+
+   const balance = await transactionsRepository.getBalance();
+
+   return response.json({transactions,balance});
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const {title,value,type, category} = request.body
+
+  const createTransactionService = new CreateTransactionService();
+
+  const transaction = await createTransactionService.execute({
+     title,
+     value,
+     type,
+     category
+  })
+
+  response.json(transaction)
+
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const deleteService = new DeleteTransactionService();
+
+  await deleteService.execute(id);
+
+  return response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
-  // TODO
+
+
 });
 
 export default transactionsRouter;
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,9 +1,19 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
-  }
-}
+  public async execute(id:string): Promise<void> {
+    const transactionsRepository =  getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if(!transaction){
+      throw new AppError('transaction not found');
+    }
+
+    await transactionsRepository.remove(transaction);
+
+  }  }
 
 export default DeleteTransactionService;
